67a6b246595a086ece9a411a957ced42a653f46c
Pavan Kumar <49811722+Pav1-kumar@users.noreply.github.com>
2024-06-19 23:11:46 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('http://localhost:4000/api/workouts', {
+      const response = await fetch('https://poets-abode-backend.onrender.com/api/workouts', {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
